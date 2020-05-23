ed5b2066a6303af50837f2b5fd60a71b34557212
NagarajM <nagarajpatilm95@gmail.com>
2020-05-23 23:57:37 +0530

    updated code

--- batchprocess/src/main/java/com/tyss/batchprocess/config/BatchConfiguration.java
+++ batchprocess/src/main/java/com/tyss/batchprocess/config/BatchConfiguration.java
@@ -2,8 +2,6 @@ package com.tyss.batchprocess.config;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -19,9 +17,6 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -29,19 +24,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import com.tyss.batchprocess.dto.Employee;
+import com.tyss.batchprocess.dto.EmployeeBean;
 import com.tyss.batchprocess.processor.EmployeeItemProcessor;
 
 import lombok.extern.java.Log;
 
 /**
  * 
- * @author CBT
+ * Configuration class where all the properties are configured
  *
  */
 @Configuration
@@ -83,65 +76,45 @@ public class BatchConfiguration {
 	 * 
 	 * @return
 	 */
-	public DataSource dataSource() {
-		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(driverClassName);
-		dataSource.setUrl(dbUrl);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
-		return dataSource;
-	}
 
 	@Bean
-	public JdbcCursorItemReader<Employee> reader() {
-		JdbcCursorItemReader<Employee> reader = new JdbcCursorItemReader<>();
+	public JdbcCursorItemReader<EmployeeBean> reader() {
+		JdbcCursorItemReader<EmployeeBean> reader = new JdbcCursorItemReader<>();
 
 		reader.setDataSource(dataSource);
 		reader.setSql(query);
 		reader.setRowMapper(new EmployeeRowMapper());
+		System.out.println("JDBCCursorItemReader");
 		return reader;
 	}
 
-	/*
-	 * @Bean public ResourcelessTransactionManager resourcelessTransactionManager()
-	 * { return new ResourcelessTransactionManager(); }
-	 * 
-	 * @Bean public MapJobRepositoryFactoryBean
-	 * mapJobRepositoryFactory(ResourcelessTransactionManager txManager) throws
-	 * Exception { MapJobRepositoryFactoryBean factory = new
-	 * MapJobRepositoryFactoryBean(txManager); factory.afterPropertiesSet(); return
-	 * factory; }
-	 */
-
-	@Scheduled(cron = "0 0-10 16 * * ?")
+	@Scheduled(cron = "0 0-10 20 * * ?")
 	public void perform() {
-
+		System.out.println("perform");
 		log.info("Job Started at :" + new java.util.Date());
 		JobParameters param = new JobParametersBuilder().addString("JobID", String.valueOf(System.currentTimeMillis()))
 				.toJobParameters();
 		try {
 			JobExecution execution = jobLauncher.run(exportEmployeeJob(), param);
-
 			log.info("Job finished with status :" + execution.getStatus());
 		} catch (Exception e) {
 			log.severe("Exception occurred while running the Batch !!! Exception is : " + e.getStackTrace());
 		}
 	}
 
-	public class EmployeeRowMapper implements RowMapper<Employee> {
+	public class EmployeeRowMapper implements RowMapper<EmployeeBean> {
 
 		@Override
-		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Employee employee = new Employee();
-			
-				employee.setMailId(rs.getString("mail_id"));
-				employee.setDateOfBirth(rs.getDate("DOB"));
-				employee.setDateOfJoin(rs.getDate("DOJ"));
-				log.info("DOB ========>" + rs.getString("DOB"));
-				log.info("DOJ ========>" + rs.getString("DOJ"));
-				log.info("mail id ======> " + rs.getString("mail_id"));
-				log.info("------------------------------------------->");
-			
+		public EmployeeBean mapRow(ResultSet rs, int rowNum) throws SQLException {
+			EmployeeBean employee = new EmployeeBean();
+
+			employee.setMailId(rs.getString("mail_id"));
+			employee.setDateOfBirth(rs.getDate("DOB"));
+			employee.setDateOfJoin(rs.getDate("DOJ"));
+			log.info("DOB ========>" + rs.getString("DOB"));
+			log.info("DOJ ========>" + rs.getString("DOJ"));
+			log.info("mail id ======> " + rs.getString("mail_id"));
+			log.info("------------------------------------------->");
 			return employee;
 		}
 	}
@@ -152,40 +125,17 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public FlatFileItemWriter<Employee> writer() {
-		FlatFileItemWriter<Employee> writer = new FlatFileItemWriter<Employee>();
-
-		writer.setResource(new ClassPathResource("Employee.txt"));
-
-		DelimitedLineAggregator<Employee> lineAggregator = new DelimitedLineAggregator<Employee>();
-		lineAggregator.setDelimiter(",");
-
-		BeanWrapperFieldExtractor<Employee> fieldExtractor = new BeanWrapperFieldExtractor<Employee>();
-		fieldExtractor.setNames(new String[] { "dateOfBirth", "dateOfJoin", "mailId" });
-		lineAggregator.setFieldExtractor(fieldExtractor);
-
-		writer.setLineAggregator(lineAggregator);
-
-		/*
-		 * writer.setLineAggregator(new DelimitedLineAggregator<Employee>() { {
-		 * setDelimiter(","); setFieldExtractor(new
-		 * BeanWrapperFieldExtractor<Employee>() { { setNames(new String[] { "mailId"
-		 * }); } });
-		 * 
-		 * } });
-		 */
-
-		return writer;
-	}
-
-	@Bean
 	public Step step1() {
-		return stepBuilderFactory.get("step1").<Employee, Employee>chunk(10).reader(reader()).processor(processor())
-				.writer(writer()).build();
+		System.out.println("Step1");
+		return stepBuilderFactory.get("step1").<EmployeeBean, EmployeeBean>chunk(10).reader(reader()).processor(processor())
+				.writer(items -> {
+					// do nothing
+				}).build();
 	}
 
 	@Bean
 	public Job exportEmployeeJob() {
+		System.out.println("exportEmployeeJob");
 		return jobBuilderFactory.get("exportEmployeeJob").incrementer(new RunIdIncrementer()).flow(step1()).end()
 				.build();
 	}
@@ -194,6 +144,8 @@ public class BatchConfiguration {
 	public SimpleJobLauncher simpleJobLauncher(JobRepository jobRepository) {
 		SimpleJobLauncher launcher = new SimpleJobLauncher();
 		launcher.setJobRepository(jobRepository);
+		System.out.println("SimpleJobLauncher");
+
 		return launcher;
 	}
 
--- batchprocess/src/main/java/com/tyss/batchprocess/processor/EmployeeItemProcessor.java
+++ batchprocess/src/main/java/com/tyss/batchprocess/processor/EmployeeItemProcessor.java
@@ -1,12 +1,14 @@
 package com.tyss.batchprocess.processor;
 
-import java.util.Date;
 import java.time.Instant;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,60 +18,83 @@ import org.springframework.web.client.RestTemplate;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-import com.tyss.batchprocess.dto.Employee;
+import com.tyss.batchprocess.dto.EmployeeBean;
 import com.tyss.batchprocess.dto.SmsAndEmailResponse;
 
 import lombok.extern.java.Log;
-
+/**
+ * 
+ * This class processes the data which is in the form of Bean object  
+ * and returns the processed data in the form of Bean object
+ */
 @Log
-public class EmployeeItemProcessor implements ItemProcessor<Employee, Employee> {
+public class EmployeeItemProcessor implements ItemProcessor<EmployeeBean, EmployeeBean> {
+	
+	@Value("${from.mailid}")
+	private String from;
+	
+	@Value("${cc.mailid}")
+	private String ccs;
+	
+	@Value("${emailservice.url}")
+	private String emailServiceUrl;
 
 	@Override
-	public Employee process(Employee employee) throws Exception {
-		
+	public EmployeeBean process(EmployeeBean employee) throws Exception {
+
+		System.out.println("Processor");
+
 		RestTemplate restTemplate = new RestTemplate();
 
 		Multimap<String, Date> multimapForDob = ArrayListMultimap.create();
 		Multimap<String, Date> multimapForDoj = ArrayListMultimap.create();
-		
-		//converting java.util.Date to java.time.LocalDate
-		Date dob=employee.getDateOfBirth();
+
+		// converting java.util.Date to java.time.LocalDate
+		Date dob = employee.getDateOfBirth();
 		Instant instant = Instant.ofEpochMilli(dob.getTime());
 		LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 		LocalDate convDob = localDateTime.toLocalDate();
-		
-		if(convDob==LocalDate.now()) {
-			log.info(""+employee.getDateOfBirth());
+
+		if (convDob == LocalDate.now()) {
+			log.info("" + employee.getDateOfBirth());
 			multimapForDob.put(employee.getMailId(), employee.getDateOfBirth());
-			
+
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-			headers.set("from", "nagarajpatilm95@gmail.com");
-			headers.set("subject", "hi  man");
-			headers.set("tos", "["+employee.getMailId()+"]");
-			headers.set("ccs", "["+"nagarajpatilm93@gmail.com"+"]");
-			headers.set("content", "b");
+			headers.set("from", from);
+			headers.set("subject", "Congratulations");
+			headers.set("tos", "[" + employee.getMailId() + "]");
+			//headers.set("ccs", "[" + "@gmail.com" + "]");
+			headers.set("ccs", ccs);
+			headers.set("content", "good night");
 			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 
-			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map,
+					headers);
+
 			SmsAndEmailResponse resp = restTemplate.postForObject("http://localhost:8082/send-email", request,
 					SmsAndEmailResponse.class);
-		}else {
-			log.info(""+employee.getDateOfJoin());
+
+		} else {
+			log.info("" + employee.getDateOfJoin());
 			multimapForDoj.put(employee.getMailId(), employee.getDateOfJoin());
-			
+
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-			headers.set("from", "nagarajpatilm95@gmail.com");
-			headers.set("subject", "hi  man");
-			headers.set("tos", "["+employee.getMailId()+"]");
-			headers.set("ccs", "["+"nagarajpatilm93@gmail.com"+"]");
-			headers.set("content", "b");
+			headers.set("from", from);
+			headers.set("subject", "congratulations");
+			System.out.println(employee.getMailId());
+			headers.set("tos", "[" + employee.getMailId() + "]");
+			headers.set("ccs", "[" + "@gmail.com" + "]");
+			headers.set("content", "Good night");
 			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 
-			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-			SmsAndEmailResponse resp = restTemplate.postForObject("http://localhost:8082/send-email", request,
+			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map,
+					headers);
+
+			SmsAndEmailResponse resp = restTemplate.postForObject(emailServiceUrl, request,
 					SmsAndEmailResponse.class);
+
 		}
 
 		log.info("mm " + multimapForDob);
